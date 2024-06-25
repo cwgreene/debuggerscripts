21d0ef123a38135462b0db088e39fbf0c83418d2
Chris Greene <archgoon@gmail.com>
2024-06-24 21:55:59 -0700

    Remove debug print

--- gdb/print_big_int.py
+++ gdb/print_big_int.py
@@ -14,19 +14,14 @@ class PrintBigInt(gdb.Command):
         inferior = gdb.selected_inferior()
         memory = inferior.read_memory(address, 16)
 
-
         data_loc_bytes =   inferior.read_memory(address, 8)
         data_loc = int.from_bytes(data_loc_bytes, byteorder='little')
-        #print(hex(data_loc))
 
         length_bytes = inferior.read_memory(address+12, 4)
         length = int.from_bytes(length_bytes, byteorder='little')
-        #print(length)
 
         digits = inferior.read_memory(data_loc, length*8)
-        #print(list(digits))
         # Print the memory in a readable format
-        print(digits.hex())
         memory_str = int.from_bytes(digits,byteorder="little")
         print(f'{memory_str}')
 
